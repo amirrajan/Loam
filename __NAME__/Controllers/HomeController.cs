d427b812319ea311e935f7b6712acf29580acf90
Amir Rajan <ar@amirrajan.net>
2012-09-22 06:47:42 -0500

    updated oak

---
+++ __NAME__/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Oak;
+using Massive;
+
+namespace __NAME__.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
